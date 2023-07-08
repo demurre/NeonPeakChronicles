332f8246fa12a864531dcb45cb7adfa5d74f5e4f
demurre <y.drozdov130705.com>
2023-07-08 21:52:40 +0300

    update

--- src/loaders/images.js
+++ src/loaders/images.js
@@ -1,21 +1,21 @@
 const imageLoader = (game) => {
-  game.load.image('background', 'assets/gamePicture/startPageBg.png');
-  game.load.image('gBackground', 'assets/gamePicture/gamePageBg.png');
-  game.load.image('mainCharacter', 'assets/gamePicture/mainHero.png');
-  game.load.image('enemy1', 'src/gamePicture/enemy1.png');
-  game.load.image('enemy2', 'src/gamePicture/enemy2.png');
-  game.load.image('enemy3', 'src/gamePicture/enemy3.png');
-  game.load.image('boss', 'src/gamePicture/finalBossBg.png');
-  game.load.image('card1', 'src/gamePicture/dmgCard1.png');
-  game.load.image('card2', 'src/gamePicture/armorCard1.png');
-  game.load.image('card3', 'src/gamePicture/healCard1.png');
-  game.load.image('card4', 'src/gamePicture/poisonCard1.png');
-  game.load.image('card5', 'src/gamePicture/weakCard1.png');
-  game.load.image('hpbar40', 'src/gamePicture/40.png');
-  game.load.image('hpbar30', 'src/gamePicture/30.png');
-  game.load.image('hpbar20', 'src/gamePicture/20.png');
-  game.load.image('hpbar10', 'src/gamePicture/10.png');
-  game.load.image('armoBar', 'src/gamePicture/armorBar.png');
+  game.load.image('background', '../assets/backgrounds/startg.png');
+  game.load.image('gBackground', '../assets/backgrounds/game.png');
+  game.load.image('mainCharacter', '../assets/hero/index.png');
+  game.load.image('enemy1', '../assets/enemies/enemy1.png');
+  game.load.image('enemy2', '../assets/enemies/enemy2.png');
+  game.load.image('enemy3', '../assets/enemies/enemy3.png');
+  game.load.image('boss', '../assets/finalBoss.png');
+  game.load.image('card1', '../assets/cards/dmg.png');
+  game.load.image('card2', '../assets/cards/armor.png');
+  game.load.image('card3', '../assets/cards/heal.png');
+  game.load.image('card4', '../assets/cards/poison.png');
+  game.load.image('card5', '../assets/cards/weak.png');
+  game.load.image('hpbar40', '../assets/bars/40.png');
+  game.load.image('hpbar30', '../assets/bars/30.png');
+  game.load.image('hpbar20', '../assets/bars/20.png');
+  game.load.image('hpbar10', '../assets/bars/10.png');
+  game.load.image('armoBar', '../assets/bars/armor.png');
 };
 
 export default imageLoader;
